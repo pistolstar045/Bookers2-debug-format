8e44f7165dbc6b82b40a9df71d86dfd56f06cc20
EC2 Default User <ec2-user@ip-172-31-39-29.ap-northeast-1.compute.internal>
2023-02-09 02:03:31 +0000

    [Update] application-6

--- rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -16,10 +16,12 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
-  get "search" => "searches#search_result"
+  get '/search', to: 'searches#search'
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
 
 
 
